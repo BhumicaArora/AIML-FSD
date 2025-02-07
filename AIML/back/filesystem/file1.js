b67e41e04a35e876171eafb5ade332749cec0fbb
Bhumica Arora <bhumicaarora16@gmail.com>
2025-02-07 09:45:09 +0530

    update

--- AIML/back/filesystem/file1.js
+++ AIML/back/filesystem/file1.js
@@ -1,4 +1,23 @@
+//const fs=require('fs');
+//const data=fs.readFileSync("./data.txt");
+//console.log(data.toString());                                string format
+//OR
+//const data=fs.readFileSync("./data.txt",{encoding:"utf-8"});  //encoding is optional
+//console.log(data);                                            //buffer format
+
+
+//fs.writeFileSync("./data3.txt"," hello this is my new work","utf-8");
+//fs.appendFileSync("./data1.txt"," work from another file","utf-8");
+//fs.renameSync("./data3.txt","./data5.txt");
+//fs.unlinkSync("./data5.txt");
+
+
 const fs=require('fs');
-const data=fs.readFileSync("./data.txt");
-console.log(data.toString());
+const data=fs.readFileSync("./data.txt","utf-8");  
+console.log(data);                                            
+if(data.match("H")){
+    console.log("H is found");
+    const newdata=data.replace("H","ABES");
+    fs.writeFileSync("./data.txt",newdata,"utf-8");
+}
 
